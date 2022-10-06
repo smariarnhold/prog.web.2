9177d59b04e8dbeb8bba8b38e7e10f4b54140f03
Maria Luiza Arnhold <101952113+smariarnhold@users.noreply.github.com>
2022-10-06 20:15:13 -0300

    Update damas2.js

--- damas2.js
+++ damas2.js
@@ -1,145 +1,145 @@
-const  tamanho Celula  =  40 ;
-deixe  pecaId  =  0 ;
-deixe  localAtual  =  80 ;
-deixe  localFuturo  =  81 ;
-deixe  classe  = '' ;
-deixe  localCaptura  =  '' ;
-deixe  jogada  =  0 ;
-documento . corpo . anexar ( criaTabuleiro ( ) ) ;
+const tamanhoCelula = 40;
+let pecaId = 0;
+let localAtual = 80;
+let localFuturo = 81;
+let classe ='';
+let localCaptura = '';
+let jogada = 0;
+document.body.append(criaTabuleiro());
 
-função  criaTabuleiro ( )  {
-     tamanho  const =  8 ;
-    deixe  tabela  =  document . createElement ( 'tabela' ) ;
+function criaTabuleiro() {
+    const tamanho = 8;
+    let tabela = document.createElement('table');
 
-    tabela . estilo . borderStyle  =  'solid' ;
-    tabela . estilo . borderSpacing  =  0 ;
-    tabela . estilo . margem  =  'auto' ;
+    tabela.style.borderStyle = 'solid';
+    tabela.style.borderSpacing = 0;
+    tabela.style.margin = 'auto';
 
-    for  ( seja  i  =  0 ;  i  <  tamanho ;  i ++ )  {
-        deixe  linha  =  documento . createElement ( 'tr' ) ;
-        tabela . anexar ( linha ) ;
-        for  ( deixe  j  =  0 ;  j  <  tamanho ;  j ++ )  {
-            deixe  cellula  =  documento . createElement ( 'td' ) ;
-			celula . setAttribute ( 'id' , ` ${ j } `  +  '-'  +  ` ${ i } ` ) ;
-            linha . anexar ( celula ) ;
-            celula . estilo . largura  =  ` ${ tamanhoCelula } px` ;
-            celula . estilo . altura  =  ` ${ tamanhoCelula } px` ;
-			pecaId  +=  1 ;
+    for (let i = 0; i < tamanho; i++) {
+        let linha = document.createElement('tr');
+        tabela.append(linha);
+        for (let j = 0; j < tamanho; j++) {
+            let celula = document.createElement('td');
+			celula.setAttribute('id',`${j}` + '-' + `${i}`);
+            linha.append(celula);
+            celula.style.width = `${tamanhoCelula}px`;
+            celula.style.height = `${tamanhoCelula}px`;
+			pecaId += 1;
 	
-            if  ( i  %  2  ==  j  %  2 )  {
-                celula . estilo . backgroundColor  =  'preto' ;
-				celula . setAttribute ( "class" , "droptarget" ) ;
-                if  ( i  *  8  +  j  <=  24 )  {
-                    celula . append ( criPeca ( 'preto' , pecaId ) ) ;
-                }  senão  if  ( i  *  8  +  j  >=  40 )  {
-                    celula . append ( criPeca ( 'red' , pecaId ) ) ;
+            if (i % 2 == j % 2) {
+                celula.style.backgroundColor = 'black';
+				celula.setAttribute("class","droptarget");
+                if (i * 8 + j <= 24) {
+                    celula.append(criaPeca('black',pecaId));
+                } else if (i * 8 + j >= 40) {
+                    celula.append(criaPeca('red',pecaId));
                 }
-            }  senão  {
-                celula . estilo . backgroundColor  =  'branco' ;
+            } else {
+                celula.style.backgroundColor = 'white';
             }
         }
-    } ;
+    };
 	
-    retornar  tabela ;	
+    return tabela;	
 }
 
-function  criaPeca ( cor , ide )  {
-		deixe  imagem  =  documento . createElement ( 'img' ) ;
-		imagem . setAttribute ( 'src' ,  `img/ ${ cor } .png` ) ;
-		imagem . setAttribute ( 'width' ,  ` ${ tamanhoCelula - 4 } px` ) ;
-		imagem . setAttribute ( 'altura' ,  ` ${ tamanhoCelula - 4 } px` ) ;
-		imagem . setAttribute ( 'arrastável' , 'true' ) ;
-		imagem . setAttribute ( 'id' ,  ide ) ;
-		imagem . setAttribute ( 'classe' ,  cor ) ;
+function criaPeca(cor,ide) {
+		let imagem = document.createElement('img');
+		imagem.setAttribute('src', `img/${cor}.png`);
+		imagem.setAttribute('width', `${tamanhoCelula-4}px`);
+		imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+		imagem.setAttribute('draggable','true');
+		imagem.setAttribute('id', ide);
+		imagem.setAttribute('class', cor);
 		
-    retornar  imagem ;
+    return imagem;
 }
 
-função  dragstart ( ) {
-	documento . addEventListener ( "dragstart" ,  function ( evento )  {
-	  evento . transferência de dados . setData ( "Texto" ,  event . target . id ) ;
-	  localAtual  =  evento . caminho [ 1 ] . identificação . toString ( ) ;
-	  classe  =  evento . caminho [ 0 ] . className ;
-	} ) ;
+function dragstart(){
+	document.addEventListener("dragstart", function(event) {
+	  event.dataTransfer.setData("Text", event.target.id);
+	  localAtual = event.path[1].id.toString();
+	  classe = event.path[0].className;
+	});
 }
 
-função  dragend ( )  {
-	documento . addEventListener ( "dragend" ,  function ( evento )  {
-	} ) ;
+function dragend() {
+	document.addEventListener("dragend", function(event) {
+	});
 }
 
-função  arrastar ( )  {
-	documento . addEventListener ( "dragover" ,  function ( evento )  {
-	  evento . preventDefault ( ) ;
-	} ) ;
+function dragover() {
+	document.addEventListener("dragover", function(event) {
+	  event.preventDefault();
+	});
 }
 
-função  drop ( ) {
-	documento . addEventListener ( "drop" ,  function ( evento )  {
-	evento . preventDefault ( ) ;
-	if  (  event . target . className  ==  "droptarget" )  {
-		const  dados  =  evento . transferência de dados . getData ( "Texto" ) ;
-		seja  c  =  evento . caminho [ 0 ] ;
-		seja  t  =  c . childElementCount ;
-		localFuturo  =  evento . alvo . identificação . toString ( ) ;
-		deixe  xa  =  localAtual . substring ( 0 , 1 ) ;
-		deixe  ya  =  localAtual . substring ( 2 , 3 ) ;
-		deixe  xf  =  localFuturo . substring ( 0 , 1 ) ;
-		deixe  yf  =  localFuturo . substring ( 2 , 3 ) ;
+function drop(){
+	document.addEventListener("drop", function(event) {
+	event.preventDefault();
+	if ( event.target.className == "droptarget") {
+		const data = event.dataTransfer.getData("Text");
+		let c = event.path[0];
+		let t = c.childElementCount;
+		localFuturo = event.target.id.toString();
+		let xa = localAtual.substring(0,1);
+		let ya = localAtual.substring(2,3);
+		let xf = localFuturo.substring(0,1);
+		let yf = localFuturo.substring(2,3);
 		
-		if ( classe  ==  'preto'  &&  xf  <  xa )  {
-			localCaptura  =  ( parseInt ( xa )  -1  ) . _ toString ( ) + "-" + ( parseInt ( ya ) + 1 ) . toString ( ) ;      
-		}  else  if ( classe  ==  'preto'  &&  xf  >  xa )  {
-			localCaptura  =  ( parseInt ( xa )  +  1 ) . toString ( )  +  "-"  +  ( parseInt ( ya )  +  1 ) . toString ( ) ;
-		}  else  if ( classe  ==  'vermelho'  &&  xf  >  xa ) {
-			localCaptura  =  ( parseInt ( xa )  +  1 ) . toString ( )  +  "-  " +  ( parseInt ( ya )  -1  ) . toString ( ) ;
-		}  else  if ( classe  ==  'vermelho'  &&  xf  <  xa ) {
-			localCaptura  =  ( parseInt ( xa )  -1  ) . _ toString ( ) + "- " + ( parseInt ( ya ) -1 ) . toString ( ) ;      
+		if(classe == 'black' && xf < xa) {
+			localCaptura = (parseInt(xa) - 1).toString() + "-" + (parseInt(ya) + 1).toString();
+		} else if(classe == 'black' && xf > xa) {
+			localCaptura = (parseInt(xa) + 1).toString() + "-" + (parseInt(ya) + 1).toString();
+		} else if(classe == 'red' && xf > xa){
+			localCaptura = (parseInt(xa) + 1).toString() + "-" + (parseInt(ya) - 1).toString();
+		} else if(classe == 'red' && xf < xa){
+			localCaptura = (parseInt(xa) - 1).toString() + "-" + (parseInt(ya) - 1).toString();
 		}
-		captura  =  documento . getElementById ( localCaptura ) ;
-		if ( captura . childElementCount  ==  '1' ) {
-			classeCapturada  =  captura . firstElementChild . className ;
-			pecaCapturada  =  captura . firstElementChild ;
+		captura = document.getElementById(localCaptura);
+		if(captura.childElementCount == '1'){
+			classeCapturada = captura.firstElementChild.className;
+			pecaCapturada = captura.firstElementChild;
 		}
-		if  ( t  ==  '0'  &&  ya  !=  yf ) {
-			if  ( classe  ==  'vermelho'  &&  ya  >  yf  &&  ya  -  yf  ==  1  &&  jogada  %  2  ==  0  ||  ya  -  yf  ==  2  &&  classeCapturada  ==  "black"  &&  jogada  %  2  ==  0  ||  classe  ==  'preto'  &&  ya  <  yf  &&  ya  -  yf  ==  - 1  && jogo  % 2  ==  1  ||  ya  -  yf  ==  - 2  &&  classeCapturada  ==  "red"  &&  jogada  %  2  ==  1 )  {
-				evento . alvo . appendChild ( document.getElementById ( data ) ) ; _ _
-				if  ( classe  ==  'vermelho'  &&  yf  ==  '0'  ||  classe  ==  'preto'  &&  yf  ==  '7' ) {
-					pecaRainha ( classe , yf , xf ) ;
+		if (t == '0' && ya != yf){
+			if (classe == 'red' && ya > yf && ya - yf == 1 && jogada % 2 == 0 || ya - yf == 2 && classeCapturada == "black" && jogada % 2 == 0 || classe == 'black' && ya < yf && ya - yf == -1 && jogada % 2 == 1 || ya - yf == -2 && classeCapturada == "red" && jogada % 2 == 1) {
+				event.target.appendChild(document.getElementById(data));
+				if (classe == 'red' && yf == '0' || classe == 'black' && yf == '7'){
+					pecaRainha(classe,yf,xf);
 				}
-				jogada  +=  1 ;
-				if ( ya  -  yf  ==  2  ||  ya  -  yf  ==  - 2 )  {
-					pecaCapturada . remover ( ) ;
-					pecaCapturada  =  '' ;
-					lasseCapturada  =  '' ;
+				jogada += 1;
+				if(ya - yf == 2 || ya - yf == -2) {
+					pecaCapturada.remove();
+					pecaCapturada = '';
+					lasseCapturada = '';
 				}
 			}
 		}
 		
 	}
-	} ) ;
+	});
 }
-function  pecaRainha ( classe ,  yf ,  xf ) {
-	local  =  xf  +  '-'  +  yf ;
-	sub  =  documento . getElementById ( local ) ;
-	pe  =  sub . firstElementChild ;
-	addId  =  pe . identificação ;
-	addClass  =  pe . className ;
-	deixe  imagem  =  documento . createElement ( 'img' ) ;
-	if  ( classe  ==  "vermelho" )  {
-		imagem . setAttribute ( 'src' ,  'img/redKing.jpeg' ) ;
-	}  senão  {
-		imagem . setAttribute ( 'src' ,  'img/blackKing.jpeg' ) ;
+function pecaRainha(classe, yf, xf){
+	local = xf + '-' + yf;
+	sub = document.getElementById(local);
+	pe = sub.firstElementChild;
+	addId = pe.id;
+	addClass = pe.className;
+	let imagem = document.createElement('img');
+	if (classe == "red") {
+		imagem.setAttribute('src', 'img/redKing.jpeg');
+	} else {
+		imagem.setAttribute('src', 'img/blackKing.jpeg');
 	}
-	imagem . setAttribute ( 'width' ,  ` ${ tamanhoCelula - 4 } px` ) ;
-	imagem . setAttribute ( 'altura' ,  ` ${ tamanhoCelula - 4 } px` ) ;
-	imagem . setAttribute ( 'arrastável' , 'true' ) ;
-	imagem . setAttribute ( 'id' ,  addId ) ;
-	imagem . setAttribute ( 'class' ,  ` ${ addClass } King` ) ;
-	pe . remover ( ) ;
-	sub . anexar ( imagem ) ;
+	imagem.setAttribute('width', `${tamanhoCelula-4}px`);
+	imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+	imagem.setAttribute('draggable','true');
+	imagem.setAttribute('id', addId);
+	imagem.setAttribute('class', `${addClass}King`);
+	pe.remove();
+	sub.append(imagem);
 }
-dragstart ( ) ;
-arrasto ( ) ;
-arrastar ( ) ;
-soltar ( ) ;
+dragstart();
+dragend();
+dragover();
+drop();
