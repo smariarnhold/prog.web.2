203f72431668b5adb6450a257ce76d13f1a4f27e
Maria Luiza Arnhold <101952113+smariarnhold@users.noreply.github.com>
2022-10-21 09:25:45 -0300

    Create ddd.js

---
+++ ddd.js
@@ -0,0 +1,19 @@
+document.querySelector("#ddd").addEventListener("change", buscaAPI);
+cidades = new Array();
+function buscaAPI(acao){
+	fetch(`https://brasilapi.com.br/api/ddd/v1/${acao.target.value}`)
+	.then(res => res.json())
+	.then(dados => {
+		cidades = dados.cities;
+		listaCidades();
+	});
+}
+function listaCidades(){
+	lista = document.querySelector('ol');
+	lista.textContent = '';
+	cidades.forEach(cidade =>{
+		item = document.createElement('li');
+		item.textContent = cidade;
+		lista.append(item);
+	});
+}
